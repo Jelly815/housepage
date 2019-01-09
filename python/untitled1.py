d53fe9da4cee59b111382554b1d1cbeb4e94be7a
Jelly <jelly.chang@hgiga.com>
2019-01-09 10:05:38 +0800

    [mod]sql

---
+++ python/untitled1.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 10 23:57:35 2018
+
+@author: Jelly
+"""
+
+import math, random
+from collections import defaultdict, Counter
+
+users_interests = [
+    ["Hadoop", "Big Data", "HBase", "Java", "Spark", "Storm", "Cassandra"],
+    ["NoSQL", "MongoDB", "Cassandra", "HBase", "Postgres"],
+    ["Python", "scikit-learn", "scipy", "numpy", "statsmodels", "pandas"],
+    ["R", "Python", "statistics", "regression", "probability"],
+    ["machine learning", "regression", "decision trees", "libsvm"],
+    ["Python", "R", "Java", "C++", "Haskell", "programming languages"],
+    ["statistics", "probability", "mathematics", "theory"],
+    ["machine learning", "scikit-learn", "Mahout", "neural networks"],
+    ["neural networks", "deep learning", "Big Data", "artificial intelligence"],
+    ["Hadoop", "Java", "MapReduce", "Big Data"],
+    ["statistics", "R", "statsmodels"],
+    ["C++", "deep learning", "artificial intelligence", "probability"],
+    ["pandas", "R", "Python"],
+    ["databases", "HBase", "Postgres", "MySQL", "MongoDB"],
+    ["libsvm", "regression", "support vector machines"]
+]
+
+def dot(v, w):
+    """v_1 * w_1 + ... + v_n * w_n"""
+    return sum(v_i * w_i for v_i, w_i in zip(v, w))
+
+
+def make_user_interest_vector(user_interests):
+    """given a list of interests, produce a vector whose i-th element is 1
+    if unique_interests[i] is in the list, 0 otherwise"""
+    return [1 if interest in user_interests else 0
+            for interest in unique_interests]
+ 
+def cosine_similarity(v, w):
+    return dot(v, w) / math.sqrt(dot(v, v) * dot(w, w))
+
+def most_similar_interests_to(interest_id):
+    similarities = interest_similarities[interest_id]
+    pairs = [(unique_interests[other_interest_id], similarity)
+             for other_interest_id, similarity in enumerate(similarities)
+             if interest_id != other_interest_id and similarity > 0]
+    return sorted(pairs,
+                  key=lambda pair: pair[1],
+                  reverse=True)
+
+def item_based_suggestions(user_id, include_current_interests=False):
+    suggestions = defaultdict(float)
+    user_interest_vector = user_interest_matrix[user_id]
+    for interest_id, is_interested in enumerate(user_interest_vector):
+        if is_interested == 1:
+            similar_interests = most_similar_interests_to(interest_id)
+            for interest, similarity in similar_interests:
+                suggestions[interest] += similarity
+
+    suggestions = sorted(suggestions.items(),
+                         key=lambda pair: pair[1],
+                         reverse=True)
+
+    if include_current_interests:
+        return suggestions
+    else:
+        return [(suggestion, weight)
+                for suggestion, weight in suggestions
+                if suggestion not in users_interests[user_id]]
+# 36項興趣
+unique_interests = sorted(list({ interest
+                                 for user_interests in users_interests
+                                 for interest in user_interests }))
+# user_interest_matrix[0]:使用者對36項是否有興趣，是:1,否:0
+user_interest_matrix = list(map(make_user_interest_vector, users_interests))
+
+# interest_user_matrix[0]:15個使用者，對於該項是否有興趣，是:1,否:0
+interest_user_matrix = [[user_interest_vector[j]
+                         for user_interest_vector in user_interest_matrix]
+                        for j, _ in enumerate(unique_interests)]
+# 使用餘弦相似度
+interest_similarities = [[cosine_similarity(user_vector_i, user_vector_j)
+                          for user_vector_j in interest_user_matrix]
+                         for user_vector_i in interest_user_matrix]
+    
+print(most_similar_interests_to(1))
