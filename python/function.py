608b6fea3abeefd22e7875e8aa85860f1e6cdf63
Jelly <ccwen815@gmail.com>
2019-01-19 01:23:41 +0800

    [add]如果有加入最愛，則把該物件加入list

--- python/function.py
+++ python/function.py
@@ -54,7 +54,7 @@ class FUNC_CLASS(DB_CONN):
             # 取得user [最後]搜尋的條件
             self.execute(user_last_sql,[user_id])
             user_last_arr = self.fetchone()
-            if(user_last_arr != None):
+            if(user_last_arr is not None):
                 user_record['last_record'] = []
                 user_record['last_record'].append([user_last_arr['area'],user_last_arr['price'],user_last_arr['ping'],user_last_arr['style'],user_last_arr['type']])
             #print(user_record['last_record'][0])
@@ -62,10 +62,11 @@ class FUNC_CLASS(DB_CONN):
             self.execute(user_often_sql,[user_id])
             user_often_arr = self.fetchall()
             
-            if user_often_arr != None:
+            if user_often_arr is not None:
                 user_record['often_record'] = []
-                for x in range(len(user_often_arr)):
-                    user_record['often_record'].append([user_often_arr[x]['area'],user_often_arr[x]['price'],user_often_arr[x]['ping'],user_often_arr[x]['style'],user_often_arr[x]['type']])
+                #for x in range(len(user_often_arr)):
+                for x, often in enumerate(user_often_arr):
+                    user_record['often_record'].append([often['area'],often['price'],often['ping'],often['style'],often['type']])
                     if user_record['last_record']:
                         diff = set(user_record['often_record'][x]).difference(set(user_record['last_record'][0]))
                     
@@ -150,80 +151,39 @@ class FUNC_CLASS(DB_CONN):
             record_arr = self.fetchall()
    
             if record_arr:
-                for record in record_arr:
-                    for x in record:
-                        new_arr[x].append(record[x])
-
+                for _, record in enumerate(record_arr):
+                    for _,val in enumerate(record):
+                        new_arr[val].append(record[val])
+            
             # 刪除離群值
             is_outlier = True
             while is_outlier:
                 outlier_index = self.get_outlier(new_arr,'item_stay_time')
-                if outlier_index != None :
+                if outlier_index is not None :
                     del record_arr[outlier_index]
                     for x in new_arr:
                         del new_arr[x][outlier_index]
                 else:
                     is_outlier = False
         
-            # 取得該User瀏覽區間
+            
             user_time_range = []
             if new_arr['item_stay_time']:
+                # 取得該User瀏覽區間
                 user_time_range = self.get_user_time_range(new_arr['item_stay_time'])
-
-            # 某user喜歡物件的時間圓餅圖
-            #if new_arr['item_stay_time']:
-                #self.plt_pie(new_arr)
+                
+                # 某user喜歡物件的時間圓餅圖
+                #if new_arr['item_stay_time']:
+                    #self.plt_pie(new_arr)
             
+                # 取得該範圍內，User有加入最愛的物件
+                is_favorite_items = self.get_is_favorite(new_arr,['main_id','add_favorite','item_stay_time'],user_time_range)
+                print(is_favorite_items)
         #except:
             #record_arr = {}
 
         return record_arr
-    """
-    def get_times_range(self,user_id,record):
-        range_arr = {}
 
-        record_sql = '''
-            SELECT  items.click_map,items.add_favorite,items.main_id,
-                    IFNULL(
-                        (SELECT SUM(`stay_time`) 
-                        FROM    `ex_record_items_stay` items_stay
-                        WHERE   items_stay.`record_items_id` = items.`id` AND
-                                `stay_time` > %s
-                    ),0) AS 'all_seconds'
-            FROM `ex_record` record,`ex_record_items` items 
-            WHERE items.`record_id` = record.`id` AND
-                  items.`user_id` = record.`user_id` AND
-                  record.`user_id` = %s AND
-                  record.`area`    = %s AND
-                  record.`price`   = %s AND
-                  record.`ping`    = %s AND
-                  record.`style`   = %s AND
-                  record.`type`    = %s AND
-                  record.`times`   > %s
-            '''
-        record_vals = [
-                setting.view_seconds,user_id,record[0],
-                record[1],record[2],record[3],
-                record[4],setting.search_times
-        ]
-        try:
-            self.execute(record_sql,record_vals)
-            record_arr = self.fetchall()
-            
-            times_arr = []
-            if record_arr:
-                for record in record_arr:
-                    times_arr.append(record['all_seconds'])
-                    times_arr.sort()
-                
-                if times_arr:
-                    # 取得中位數的前後值
-                    range_arr = self.get_median_range(times_arr)   
-        except:
-            range_arr = {}
-
-        return range_arr
-    """
     # 取得中位數的前後值
     def get_median_range(self,times_arr):
         range_arr = {}
@@ -264,7 +224,8 @@ class FUNC_CLASS(DB_CONN):
         
         median_num = round(len(data['item_stay_time']))
         
-        for zero in range(len(data['item_stay_time'])):
+        #for zero in range(len(data['item_stay_time'])):
+        for zero in enumerate(data['item_stay_time']):
             explode.append(0.1 if (zero + 1) == median_num else 0)
         
         fig1, ax1 = plt.subplots()
@@ -286,32 +247,31 @@ class FUNC_CLASS(DB_CONN):
   
         # 刪除為True的資料
         if not df.empty:
-                for x in range(len(df)):
-                    this_bool = df.iloc[x]['Outlier']
-                    if this_bool:
-                        del_index = x
+            for x in range(len(df)):
+                this_bool = df.iloc[x]['Outlier']
+                if this_bool:
+                    del_index = x
         
         return del_index
     
-    # 取得該User瀏覽區間
+    # 取得該User瀏覽時間的中位數
     def get_user_time_range(self,data):
         time_range = {'low':0,'high':0}
         
         if data:
-           time_range['low'] = self.quantile(data,0.5)
-           time_range['high'] = self.quantile(data,0.99)
+           time_range = self.quantile(data,0.5)
         
         return time_range
         
-    # 取得該User是否有加入最愛的習慣
-    def get_is_favorite(self,user_id):
-        return ''
-'''
-import function
-x = function.DB_CONN()
-
-AA = x.execute("SELECT `id` FROM `ex_user` WHERE `unid` = 'm199cdc39ee6e65811960a187ccf1fcb9'")
-rows = x.fetchall()    # get all selected rows, as Barmar mentioned
-for r in rows:
-    print(r)
-'''
+    # 取得該範圍內，User有加入最愛的物件
+    def get_is_favorite(self,data,columns_list,time_range):
+        items_arr   = []
+        df = pd.DataFrame(data, columns=columns_list)
+        df = df[df['item_stay_time'] >= time_range]
+        df_favorite = df[df.add_favorite == 1]
+        # 如果有加入最愛，則把該物件加入list
+        if not df_favorite.empty:
+            for x in df_favorite.index:
+                items_arr.append(df['main_id'][x])
+                 
+        return list(set(items_arr))
