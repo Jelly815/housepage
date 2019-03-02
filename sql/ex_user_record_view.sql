1b081e900b0e9dc715893c0b25bf71c0968c5368
Jelly <ccwen815@gmail.com>
2019-03-02 22:38:54 +0800

    [mod]ex_record_items_stay.type_key type_value

--- sql/ex_user_record_view.sql
+++ sql/ex_user_record_view.sql
@@ -4,16 +4,16 @@ SELECT `record`.`id` AS `record_id`,`record`.`user_id` AS `user_id`,`record`.`ar
 `record`.`type` AS `type`,`record`.`times` AS `record_times`,`items`.`main_id`,
 `items`.`times` AS `items_times`,`items`.`click_map` AS `click_map`,
 `items`.`add_favorite` AS `add_favorite`,`items`.`last_time` AS `last_time`,
-`items_stay`.`stay_time` AS `item_stay_time`,`items_map`.`stay_time` AS `map_stay_time`
+`items_stay`.`type_value` AS `item_stay_time`,`items_map`.`stay_time` AS `map_stay_time`
 from (`ex_record` `record` join ((`ex_record_items` `items`
 
 LEFT JOIN `ex_record_items_stay` `items_stay`
-ON((`items_stay`.`record_items_id` = `items`.`id`)))
+ON((`items_stay`.`record_items_id` = `items`.`id` AND `items_stay`.`type_key`='stay_time')))
 
 LEFT JOIN `ex_record_items_map` `items_map`
 ON((`items_map`.`record_items_id` = `items`.`id`))))
 
 WHERE (`record`.`id` = `items`.`record_id` AND `items`.`times` > 1 AND
-	`items_stay`.`stay_time` > 5 AND `items`.`last_time` >= (NOW() - INTERVAL 180 DAY) AND
+	`items_stay`.`type_value` > 5 AND `items`.`last_time` >= (NOW() - INTERVAL 180 DAY) AND
 	(SELECT COUNT(`id`) FROM `ex_main` WHERE `is_closed` = 0 AND `id` = `items`.`main_id`) > 0
 )
