cd33b5de3df2d7c76b68f125f4c3dc37238ebbae
Jelly <ccwen815@gmail.com>
2019-03-24 23:14:09 +0800

    [add]排成建立使用者，喜歡物件:在意的項目，不喜歡物件:與喜歡項目裡面的內容完全不同的項目

--- cron/user_cron_items.php
+++ cron/user_cron_items.php
@@ -141,100 +141,278 @@ foreach ($get_user as $key => $get_user_value) {
     );
 
     $db->insert_table_data('ex_record_items_obj',$no_like_data);
-}
 
-$get_user   = $db->select_table_data('ex_record_items_obj','*',
-    array(array(0,'user_id','=','m1b414f0be20777c30e0423f441b09db8'),array(0,'is_like','=',1)));
+    // User可能喜歡的物件
+    $get_user   = $db->select_table_data('ex_record_items_obj','*',
+                    array(array(0,'user_id','=',$user_id),array(0,'is_like','=',1)));
+    $user_items = array();
+    foreach ($get_user as $key => $value) {
+        $item_matrix = array();
 
-foreach ($get_user as $key => $value) {
-    $item_matrix = array();
+        $user_items = json_decode($value['items']);
 
-    if($value['is_like'] == 1){
-        echo "<pre>";print_r('=====喜歡=====');echo "</pre>";
-    }else{
-        echo "<pre>";print_r('=====不喜歡=====');echo "</pre>";
+        // 檢查community
+        array_push($item_matrix, similar_matrix_value($user_items->community));
+
+        // 檢查status
+        array_push($item_matrix, similar_matrix_value($user_items->status));
+
+        // 檢查around
+
+        // 檢查description
+        array_push($item_matrix, range_matrix_value($user_items->description));
+
+        // 檢查price
+        array_push($item_matrix, range_matrix_value($user_items->price));
+
+        // 檢查unit
+        array_push($item_matrix, range_matrix_value($user_items->unit));
+
+        // 檢查builder
+        array_push($item_matrix, similar_matrix_value($user_items->builder));
+
+        // 檢查fee
+        array_push($item_matrix, range_matrix_value($user_items->fee));
+
+        // 檢查direction
+        array_push($item_matrix, similar_matrix_value($user_items->direction));
+
+        // 檢查type
+        array_push($item_matrix, similar_matrix_value($user_items->type));
+
+        // 檢查floor
+        array_push($item_matrix, similar_matrix_value($user_items->floor));
+
+        // 檢查age
+        array_push($item_matrix, range_matrix_value($user_items->age));
+
+        // 檢查parking
+        array_push($item_matrix, similar_matrix_value($user_items->parking));
+
+        // 檢查ping
+        array_push($item_matrix, range_matrix_value($user_items->ping));
+
+        // 檢查room
+        array_push($item_matrix, similar_matrix_value($user_items->room));
+
+        // 檢查area
+        array_push($item_matrix, similar_matrix_value($user_items->area));
+
+        // 寫入喜歡物件(在意的項目)
+        $like_matrix_data   = array(
+            'user_id'   => $user_id,
+            'items'     => json_encode($item_matrix),
+            'is_like'   => 3,
+        );
+
+        $db->insert_table_data('ex_record_items_obj',$like_matrix_data);
     }
-    $user_items = json_decode($value['items']);
-echo '<pre>';print_r($user_items);echo '</pre>';
-    // 檢查community
-    array_push($item_matrix, similar_matrix_value($user_items->community));
 
-    // 檢查status
-    array_push($item_matrix, similar_matrix_value($user_items->status));
+    // User可能不喜歡的物件
+    $user_items2 = array();
+    $get_user2   = $db->select_table_data('ex_record_items_obj','*',
+                    array(array(0,'user_id','=',$user_id),array(0,'is_like','=',0)));
+    foreach ($get_user2 as $key => $value) {
+        $item_matrix2 = array();
 
-    // 檢查around
+        $user_items2 = json_decode($value['items']);
 
-    // 檢查description
-    array_push($item_matrix, range_matrix_value($user_items->description));
+        // 檢查community
+        if($item_matrix[0] == 0){
+            array_push($item_matrix2, 0);
+        }else{
+            array_push($item_matrix2, similar_matrix_value($user_items->community,$user_items2->community));
+        }
 
-    // 檢查price
-    array_push($item_matrix, range_matrix_value($user_items->price));
+        // 檢查status
+        if($item_matrix[1] == 0){
+            array_push($item_matrix2, 0);
+        }else{
+            array_push($item_matrix2, similar_matrix_value($user_items->status,$user_items2->status));
+        }
 
-    // 檢查unit
-    array_push($item_matrix, range_matrix_value($user_items->unit));
+        // 檢查around
 
-    // 檢查builder
-    array_push($item_matrix, similar_matrix_value($user_items->builder));
+        // 檢查description
+        if($item_matrix[2] == 0){
+            array_push($item_matrix2, 0);
+        }else{
+            array_push($item_matrix2, range_matrix_value_nolike($user_items->description,$user_items2->description));
+        }
 
-    // 檢查fee
-    array_push($item_matrix, range_matrix_value($user_items->fee));
+        // 檢查price
+        if($item_matrix[3] == 0){
+            array_push($item_matrix2, 0);
+        }else{
+            array_push($item_matrix2, range_matrix_value_nolike($user_items->price,$user_items2->price));
+        }
 
-    // 檢查direction
-    array_push($item_matrix, similar_matrix_value($user_items->direction));
+        // 檢查unit
+        if($item_matrix[4] == 0){
+            array_push($item_matrix2, 0);
+        }else{
+            array_push($item_matrix2, range_matrix_value_nolike($user_items->unit,$user_items2->unit));
+        }
 
-    // 檢查type
-    array_push($item_matrix, similar_matrix_value($user_items->type));
+        // 檢查builder
+        if($item_matrix[5] == 0){
+            array_push($item_matrix2, 0);
+        }else{
+            array_push($item_matrix2, similar_matrix_value($user_items->builder,$user_items2->builder));
+        }
 
-    // 檢查floor
-    array_push($item_matrix, similar_matrix_value($user_items->floor));
+        // 檢查fee
+        if($item_matrix[6] == 0){
+            array_push($item_matrix2, 0);
+        }else{
+            array_push($item_matrix2, range_matrix_value_nolike($user_items->fee,$user_items2->fee));
+        }
 
-    // 檢查age
-    array_push($item_matrix, range_matrix_value($user_items->age));
+        // 檢查direction
+        if($item_matrix[7] == 0){
+            array_push($item_matrix2, 0);
+        }else{
+            array_push($item_matrix2, similar_matrix_value($user_items->direction,$user_items2->direction));
+        }
 
-    // 檢查parking
-    array_push($item_matrix, similar_matrix_value($user_items->parking));
+        // 檢查type
+        if($item_matrix[8] == 0){
+            array_push($item_matrix2, 0);
+        }else{
+            array_push($item_matrix2, similar_matrix_value($user_items->type,$user_items2->type));
+        }
 
-    // 檢查ping
-    array_push($item_matrix, range_matrix_value($user_items->ping));
+        // 檢查floor
+        if($item_matrix[9] == 0){
+            array_push($item_matrix2, 0);
+        }else{
+            array_push($item_matrix2, similar_matrix_value($user_items->floor,$user_items2->floor));
+        }
 
-    // 檢查room
-    array_push($item_matrix, similar_matrix_value($user_items->room));
+        // 檢查age
+        if($item_matrix[10] == 0){
+            array_push($item_matrix2, 0);
+        }else{
+            array_push($item_matrix2, range_matrix_value_nolike($user_items->age,$user_items2->age));
+        }
 
-    // 檢查area
-    array_push($item_matrix, similar_matrix_value($user_items->area));
+        // 檢查parking
+        if($item_matrix[11] == 0){
+            array_push($item_matrix2, 0);
+        }else{
+            array_push($item_matrix2, similar_matrix_value($user_items->parking,$user_items2->parking));
+        }
 
-    // 寫入喜歡物件(在意的項目)
-    $like_matrix_data   = array(
-        'user_id'   => $user_id,
-        'items'     => json_encode($item_matrix),
-        'is_like'   => 3,
-    );
-echo '<pre>';print_r($like_matrix_data);echo '</pre>';
-    //$db->insert_table_data('ex_record_items_obj',$like_matrix_data);
+        // 檢查ping
+        if($item_matrix[12] == 0){
+            array_push($item_matrix2, 0);
+        }else{
+            array_push($item_matrix2, range_matrix_value_nolike($user_items->ping,$user_items2->ping));
+        }
+
+        // 檢查room
+        if($item_matrix[13] == 0){
+            array_push($item_matrix2, 0);
+        }else{
+            array_push($item_matrix2, similar_matrix_value($user_items->room,$user_items2->room));
+        }
+
+        // 檢查area
+        if($item_matrix[14] == 0){
+            array_push($item_matrix2, 0);
+        }else{
+            array_push($item_matrix2, similar_matrix_value($user_items->area,$user_items2->area));
+        }
+
+        // 寫入不喜歡物件(在意的項目)
+        $like_matrix_data   = array(
+            'user_id'   => $user_id,
+            'items'     => json_encode($item_matrix2),
+            'is_like'   => 4,
+        );
+
+        $db->insert_table_data('ex_record_items_obj',$like_matrix_data);
+    }
 }
 
 // 若大於一半有相似的，回傳1，否則回傳0
-function similar_matrix_value($val_arr){
-    $org_count = round(count($val_arr) / 2);
-    $chk_count = count(array_unique($val_arr));
+function similar_matrix_value($val_arr,$compare=array()){
+    $org_count  = round(count($val_arr) / 2);
+    $return_val = 0;
+    if(!empty($compare)){
+        $val_arr=array_diff($val_arr,$compare);
+    }
 
-    return ($org_count >= $chk_count)?1:0;
+    $chk_count  = count(array_unique($val_arr));
+
+    if(!empty($compare)){
+        $return_val     = ($chk_count != 0 && $org_count >= $chk_count)?0:1;
+    }else{
+        $return_val     = ($chk_count != 0 && $org_count >= $chk_count)?1:0;
+    }
+
+    return $return_val;
 }
 
 function range_matrix_value($val_arr){
-    $avg    = array_sum($val_arr) / count($val_arr);
+    $get_range  = get_range($val_arr);
+
+    $avg    = (count($val_arr) != 0)?array_sum($val_arr) / count($val_arr):0;
     $avg    = round($avg, 3);
 
     $dev    = stats_standard_deviation($val_arr);
 
-    $start_avg  = (double)($avg - $dev);
-    $end_avg    = (double)($avg + $dev);
+    $start_avg  = $get_range[0];
+    $end_avg    = $get_range[1];
 
     $value_sum  = 0;
     foreach ($val_arr as $value) {
-        $value_sum += ($start_avg <= $value && $value <= $end_avg)?1:0;
+        $value_sum += ($start_avg != 0 && $end_avg != 0 && $start_avg <= $value && $value <= $end_avg)?1:0;
     }
 
-    return (round(count($val_arr) / 2) <= $value_sum)?1:0;
+    return (count($val_arr) != 0 && round(count($val_arr) / 2) <= $value_sum)?1:0;
+}
+
+function range_matrix_value_nolike($val_arr,$compare=array()){
+    $return_val = 0;
+
+    if(!empty($compare)){
+        sort($val_arr);
+
+        $mid        = (!empty($val_arr))?(int)floor(count($val_arr) / 2):0;
+        $get_range  = get_range($val_arr);
+        $start_avg  = $get_range[0];
+        $end_avg    = $get_range[1];
+
+        $value_sum  = 0;
+
+        foreach ($compare as $key => $value) {
+            if(!empty($get_range)){
+                $value_sum += ($start_avg <= $value && $value <= $end_avg)?1:0;
+            }
+        }
+        $return_val = ($value_sum < $mid)?1:0;
+    }
+
+    return $return_val;
+}
+// 取得陣列區間範圍
+function get_range($arr){
+    $start_avg  = (double)0;
+    $end_avg    = (double)0;
+
+    if(!empty($arr)){
+        $avg    = array_sum($arr) / count($arr);
+        $avg    = round($avg, 3);
+
+        $dev    = stats_standard_deviation($arr);
+
+        $start_avg  = (double)($avg - $dev);
+        $end_avg    = (double)($avg + $dev);
+    }
+
+    $return_val = array($start_avg,$end_avg);
+
+    return $return_val;
 }
 ?>
