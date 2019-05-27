cb4dba3a6314daa40511c0096902fb31d30b3e1b
Jelly <ccwen815@gmail.com>
2019-05-28 00:13:24 +0800

    fix data

--- fix_data.php
+++ fix_data.php
@@ -8,17 +8,21 @@ include_once(__DIR__.'/lib/lang.php');
 
 $db 	= new db_function();
 
-$str 	= array('房','廳','衛');
+#$str 	= array('房','廳','衛');
+
 $main_data  = $db->select_table_data('ex_main',
         array('id','style'),
-        array(array(3,"style LIKE '%房%'")));
-echo "<pre>";print_r($main_data);echo "</pre>";exit;
+        array(array(3,"direction = ''")));
+#echo "<pre>";print_r($main_data);echo "</pre>";exit;
 foreach ($main_data as $key => $value) {
-	foreach ($str as $str_val) {
-		$value['style'] = str_replace($str_val,";",$value['style']);
-	}
-	$up_record_sql  = "UPDATE `ex_main` SET `style` = ? WHERE `id`= ? ";
-    $vals_arr   = array($value['style'],$value['id']);
+	#foreach ($str as $str_val) {
+		#$value['style'] = str_replace($str_val,";",$value['style']);
+		$value['direction'] = rand(1, 8);
+	#}
+	#$up_record_sql  = "UPDATE `ex_main` SET `style` = ? WHERE `id`= ? ";
+    #$vals_arr   = array($value['style'],$value['id']);
+    $up_record_sql  = "UPDATE `ex_main` SET `direction` = ? WHERE `id`= ? ";
+    $vals_arr   = array($value['direction'],$value['id']);
 
     $result     = $db->update_data($up_record_sql,$vals_arr);
 }
